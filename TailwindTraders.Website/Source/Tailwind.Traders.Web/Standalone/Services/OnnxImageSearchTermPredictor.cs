3e486bf5fc5adc22085bba6bd13039f7266815ef
neupanes39 <angela@dhangadhi.store>
2025-06-24 16:28:24 +1200

    changestoomini

--- TailwindTraders.Website/Source/Tailwind.Traders.Web/Standalone/Services/OnnxImageSearchTermPredictor.cs
+++ TailwindTraders.Website/Source/Tailwind.Traders.Web/Standalone/Services/OnnxImageSearchTermPredictor.cs
@@ -22,4 +22,42 @@ namespace Tailwind.Traders.Web.Standalone.Services
             session = new InferenceSession(filePath);
         }
 
-        public Task<st
+        public Task<string> PredictSearchTerm(Stream imageStream)
+        {
+            DenseTensor<float> data = ConvertImageToTensor(imageStream);
+            var input = NamedOnnxValue.CreateFromTensor<float>("data", data);
+            using var output = session.Run(new[] { input });
+            var prediction = output.First(i => i.Name == "classLabel").AsEnumerable<string>().First();
+            return Task.FromResult(prediction);
+        }
+
+        private DenseTensor<float> ConvertImageToTensor(Stream imageStream)
+        {
+            var data = new DenseTensor<float>(new[] { 1, 3, 224, 224 });
+
+            using (var image = Image.Load<Rgba32>(imageStream))
+            {
+                image.Mutate(ctx => ctx.Resize(new ResizeOptions
+                {
+                    Size = new Size(224, 224),
+                    Mode = ResizeMode.Stretch
+                }));
+
+                for (int y = 0; y < image.Height; y++)
+                {
+                    var pixelRowSpan = image.GetPixelRowSpan(y);
+                    for (int x = 0; x < image.Width; x++)
+                    {
+                        var color = pixelRowSpan[x];
+                        // ONNX model often expects channels in order: R, G, B (adjust if your model expects BGR)
+                        data[0, 0, y, x] = color.R / 255f; // Normalize pixel to [0,1]
+                        data[0, 1, y, x] = color.G / 255f;
+                        data[0, 2, y, x] = color.B / 255f;
+                    }
+                }
+            }
+
+            return data;
+        }
+    }
+}
