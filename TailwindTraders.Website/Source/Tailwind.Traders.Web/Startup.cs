6461b466914a2eb385bf04a9499eb006f0c29930
neupanes39 <angela@dhangadhi.store>
2025-06-24 15:25:42 +1200

    added everything new

--- TailwindTraders.Website/Source/Tailwind.Traders.Web/Startup.cs
+++ TailwindTraders.Website/Source/Tailwind.Traders.Web/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Tailwind.Traders.Web.Standalone;
-//testing flow test
+
 namespace Tailwind.Traders.Web
 {
     public class Startup
